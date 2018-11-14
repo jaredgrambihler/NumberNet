f1d6433858098d2d6eec7d81d98d748f3d812a1f
JaredGrambihlerHome <jaredgrambihler@gmail.com>
2018-11-13 21:14:25 -0800

    Added main w/ sudocode and MNIST dataset

---
+++ main.py
@@ -0,0 +1,51 @@
+import Layers as ly
+import numpy as np
+
+#class to create the network and store it in memory
+class Network():
+
+    def __init__(self):
+        #init weights
+        #init network
+        #call output for initial accuracies
+        pass
+
+    def forwardPass(self, image):
+        #run a forward pass of the network w/ an image
+        #return label
+        pass
+
+    def train(self, miniBatch):
+        #train network on minibatch of data
+        #update weights
+        #call for data to be output
+        pass
+
+    def accuracy(self, testData):
+        #run network pass on test data
+        pass
+
+    def outputData(self):
+        #output loss
+        #output accuracy
+        #output weights
+        #output train time
+        pass
+
+
+def importData():
+    #imports dataset
+    #might need to make an object to handle the data
+    pass
+
+
+def main():
+    #init network
+    #import data
+    #train network w/ minibatches
+    #save weights!
+    #test final accuracy
+    #display output data
+    pass
+
+
