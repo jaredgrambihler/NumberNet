22618fe5c11c0d744a915b06992c4fb82baf1ff7
JaredGrambihlerHome <jaredgrambihler@gmail.com>
2018-11-29 20:16:55 -0800

    updated comments

--- Layers.py
+++ Layers.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 
+
 class Layer:
     
     #defines init for all methods to be a pass. Variables should be created
@@ -16,36 +17,50 @@ class Layer:
     def forwardPass(self):
         pass
 
+
     #to be implemented for each layer. Recieves a gradient as input, returns
     #a gradient to be passed back to prior layer. If there are several inputs,
     #the values are returned in order of the input #'s
     def backwardPass(self, priorGradient):
         pass
 
-    #fixes error for 2 gradients combining.
+
+    #fixes error for 2 gradients combining in backprop
     def backwardPass(self, gradient1, gradient2):
         gradient = gradient1 + gradient2
         backwardPass(gradient)
+
 
 
 #class for weights
 class Weights:
 
     #random init of weights for x,y dimensions
-    def __init__(self, x, y):
-        self.weights = np.random.randn(x, y) * 10e-3
+    #takes an optional distribution size for determining
+    #the spread of the normal distribution
+    def __init__(self, x, y, distribution = 10e-3):
+        self.weights = np.random.randn(x, y) * distribution
+
 
     #backwardPass for gradients on weights
+    #this method is irrelevant for a weights matrix
     def backwardPass(self, priorGradient):
         pass
 
-    #updates the weight gradient based on avg of minibatch
-    def updateGrad(self, stepSize, avgGrad):
-        self.weights -= avgGrad * stepSize
-        weightSum = sum(sum(abs(self.weights))) #takes sum of abs of weight
-        self.weights = self.weights / (weightSum * 78400) #prevents weights from exploding, should be fixed later
+
+    #updates the weight matrix based on a gradient and stepsize
+    def updateGrad(self, stepSize, grad):
+        self.weights -= grad * stepSize
+        #takes sum using abs values
+        weightSum = sum(sum(abs(self.weights)))
+        #prevents weights from exploding using the sum, should be fixed later
+        self.weights = self.weights / (weightSum * 78400)
+
+
 
 #unique last layer to be used to handle the loss.
+#might not be necesseray, depending on how output
+#data is coded.
 class Loss(Layer):
 
     #doesn't need to pass forward.
@@ -53,73 +68,91 @@ class Loss(Layer):
         self.loss = input1
         #outputs data
 
+
     #gradient of the loss w/ respect to itself is always 1.
     def backwardPass(self):
         return 1.00
     
 
+
+#adds too matrices together. Useful for biases.
+#HAS NOT BEEN TESTED
 class Addition(Layer):
 
     #adds two vectors
     def forwardPass(self, input1, input2):
        return input1 + input2
     
+
     #passes back gradient
     def backwardPass(self, priorGradient):
         return priorGradient
 
 
+
+#class for subtractice matrices.
+#HAS NOT BEEN TESTED
 class Subtraction(Layer):
 
     #first input vector is the first in operation.
     def forwardPass(self, input1, input2):
         return input1-input2
 
+
     #flips the gradient for subtracted element, passes back gradient for first element. 
     def backwardPass(self, priorGradient):
         return priorGradient, priorGradient*-1
 
 
+
 #computes a scalar operation
+#HAS NOT BEEN TESTED
 class Scalar(Layer):
     
     #takes a vector input and a scalar float value
     def forwardPass(self, input1, scalar):
+        #saves scalar for backprop
         self.scalar = scalar
         return scalar * input1
+
 
     #returns scaled vector gradient
     def backwardPass(self, priorGradient):
         return self.scalar * priorGradient
 
 
-#dot product b/w two vectors
+
+#dot product b/w two vectors. Currently won't work if
+#the input2 is a 2D vector. input1 is weights, input2
+#is the image/previous layer. Need to be implemented
+#to work with more than 1D input2 vectors.
 class Multiplication(Layer):
     
-    #stores inputs and returns the dot product
+    #Returns the dot product
     def forwardPass(self, input1, input2):
+        #stores inputs, gets the weights matrix from weight layer passed in.
         self.input1 = input1.weights
         self.input2 = input2
         return self.input1.dot(self.input2)
 
 
-    #returns grad of input1 and input2
+    #returns grad only for input1
     def backwardPass(self, priorGradient):
-        #scale vector for each row based on the dot product effect it had.
-        #It is important that input2 is 1D.
-##        returnArray = np.vstack((self.input1[0,:], self.input1[1,:]))
-##        for i in range(self.input1.shape[0]-2):
-##            returnArray = np.vstack((returnArray, self.input1[i+2,:])) 
-##        return returnArray , 0
-        weightGrad = np.zeros(self.input1.shape) #creates an array for the gradients that is the same shape as the weights
-        for i in range(len(priorGradient)): #for the number of previous gradients
+        #creates an array for the gradients that is the same shape as the weights
+        weightGrad = np.zeros(self.input1.shape)
+        #loops through number of previous gradients
+        for i in range(len(priorGradient)):
+            #loops through all previous inputs of 1d vector
             for j in range(len(self.input2)):
+                #adds to the weightGrad the priorGradient * 1d value for that element
+                #Creates a weightGrad matrix with all derivatives
                 weightGrad[i][j] = priorGradient[i] * self.input2[j]
-        return weightGrad, 0 #the 0 is for the other array, not needed currently
+        return weightGrad
 
 
 
 #1/x division
+#HAS NOT BEEN TESTED
 class Division(Layer):
 
     #stores input1 and returns 1/input1
@@ -127,24 +160,30 @@ class Division(Layer):
         self.input1 = input1
         return 1/input1
 
+
     #returns prior gradient times -1/x^2
     def backwardPass(self, priorGradient):
         return priorGradient * -1/(self.input1**2)
 
 
+
 #computes max as a sum of the total array
+#HAS NOT BEEN TESTED
 class Max(Layer):
 
     #returns whatever array has a greater sum
     def forwardPass(self, input1, input2):
-        if(input1.sum() > input2.sum()):
+        #sums both arrays
+        if(sum(input1) > sum(input2)):
             self.maxVal = True #these true/false refer to 1/2 input being true. This saves computation in the backwards pass.
             return input1
         else:
             self.maxVal = False
             return input2
 
+
     #routes gradient to max value
+    #returns a 0 or the gradient matrix.
     def backwardPass(self, priorGradient):
         if(self.maxVal):
             return priorGradient, 0
@@ -152,10 +191,12 @@ class Max(Layer):
             return 0, priorGradient
 
 
+
 #natural log
+#HAS NOT BEEN TESTED
 class Log(Layer):
 
-    #takes ln(x), stores input 
+    #returns ln(x), stores input 
     def forwardPass(self, input1):
         self.input1 = input1
         return np.log(input1)
@@ -165,19 +206,26 @@ class Log(Layer):
         return priorGradient * 1/self.input1
 
 
+
 #e^x
+#HAS NOT BEEN TESTED
 class Exp(Layer):
 
     #exponentiates the input
     def forwardPass(self, input1):
-        self.backwardPass = np.exp(input1) #since derivative is equal to e^x, stores it to save compute
+        #since derivative is equal to e^x, stores it to save compute
+        self.backwardPass = np.exp(input1)
         return self.backwardPass
+
 
     #returns grad * previously computed e^x value
     def backwardPass(self, priorGradient):
         return self.backwardPass * priorGradient
 
 
+
+#ReLU activation function
+#HAS NOT BEEN TESTED
 class ReLU(Layer):
 
     #forces negative values to 0
@@ -185,30 +233,52 @@ class ReLU(Layer):
         self.result = np.maximum(0, input1)
         return self.result
 
+
     #sets not activated values to 0 for the gradient
     def backwardPass(self, priorGradient):
         #where self.result is 0, grad goes to 0
-        multgrad = np.ones(self.result.shape) #sets values to 1
-        multgrad *= self.result #forces all 0 values to 0
-        return multgrad * priorGradient #all values are preserves *1 or forced to 0
+        #sets return array values to 1, same size as priorGradient
+        multgrad = np.ones(priorGradient.shape)
+        #forces 0 values to 0 for return gradient
+        multgrad *= self.result 
+        #all values are preserves *1 or forced to 0
+        return multgrad * priorGradient
 
 
+
+#computes the Softmax loss
 class Softmax(Layer):
 
     #computes and returns a softmax loss
     def forwardPass(self, input1, labelIndex):
-        self.labelIndex = labelIndex #saves index for backwardPass
-        self.expValue = np.exp(input1) #exponentiates
-        self.sum = np.sum(self.expValue) #takes sum
-        self.normalizedValue = self.expValue[labelIndex] / self.sum #saves compute by skipping to label index
+        #saves index for backwardPass
+        self.labelIndex = labelIndex
+        #exponentiates, can easily overflow (exploding gradients)
+        self.expValue = np.exp(input1)
+        #takes sum to normalize
+        self.sum = np.sum(self.expValue)
+        #saves compute by skipping to label index, ignores values that will go to 0
+        #this might have to be changed to preserve backwards gradient flow
+        self.normalizedValue = self.expValue[labelIndex] / self.sum
+        #computes loss as -log of the normalized values for the label score
         self.loss = -math.log(self.normalizedValue)
         return self.loss
     
+
     #returns loss of softmax function (only one element is used since label is one element)
+    #priorGradeint is 1.00 by default (dL/dL should always be 1.00), but can be modified
+    #if needed.
     def backwardPass(self, priorGradient = 1.00):
+        #finds gradient for the ln(x) portion
         grad = priorGradient * -1/self.normalizedValue
+        #updates gradient to scale based on the sum
         grad /= self.sum
+        #updates grad for e^x
         grad *= self.expValue[self.labelIndex]
-        returnGrad = np.zeros(self.expValue.shape) #makes a zeros array for return
-        returnGrad[self.labelIndex] = grad #set the value of the gradient
+         #makes a zeros array for return
+         #MAY BE PROBLEMATIC
+        returnGrad = np.zeros(self.expValue.shape)
+        #set the value of the gradient for the single gradient
+        #once again, may be problematic
+        returnGrad[self.labelIndex] = grad
         return returnGrad
--- main.py
+++ main.py
@@ -3,59 +3,84 @@ import numpy as np
 from mnist import MNIST
 import time
 
-#class to create the network and store it in memory
+
+#class to create the network and store it in memory. Can be pickled to avoid retraining the network.
 class Network():
 
+    #initializes the class with an optionally defined step size.
+    #the layers that are stored must be written here, although
+    #the interactions b/w layers are in the forward and backward pass
+    #methods.
     def __init__(self, stepSize = 10e-5):
-        #init weights
+        #init weights, size is (labelNumbers, imagePixels)
         self.w1 = ly.Weights(10, 784)
-        #init network
+        #init network layers
+        #this is a single layer network w/ no activation function.
         self.l1 = ly.Multiplication()
         self.loss = ly.Softmax()
         self.stepSize = stepSize
 
 
+    #forwards an image throught the network and outputs the loss.
+    #saves the score outputs to the class.
     def forwardPass(self, image, labelIndex):
-        #run a forward pass of the network w/ an image
+        #layer1 forward pass, happens to be only layer.
         self.h1 = self.l1.forwardPass(self.w1, image)
+        #loss function forward pass.
         self.loss.forwardPass(self.h1, labelIndex)
 
 
+    #backprops through the network, using values stored in the layers.
+    #returns the gradient of the weight classes.
     def backwardPass(self):
-        #backprop through the network
-        loss1 = self.loss.backwardPass()
-        loss2, toss = self.l1.backwardPass(loss1) #loss2 is the loss of the weights
-        return loss2
+        grad1 = self.loss.backwardPass()
+        grad2 = self.l1.backwardPass(grad1)
+        return grad2
 
 
+    #trains the network on a minibatch of data.
+    #updates the gradient after find the avg gradient for the
+    #entire minibatch.
     def trainBatch(self, miniBatchImages, miniBatchLabels):
-        #train network on minibatch of data
-        #forward/backprop through each single image
-        dW = np.zeros((10, 784)) #same size as weights
-        numData = len(miniBatchImages) #stores number of images being tested
-        for i in range(numData): #runs through the entire miniBatch
+        #initializes return array the same size as weights
+        dW = np.zeros(self.w1.weights.shape)
+         #stores number of images being tested
+        numData = len(miniBatchImages)
+        #runs through the miniBatch
+        for i in range(numData):
+            #forwards a single image and label through the network.
             self.forwardPass(miniBatchImages[i], miniBatchLabels[i])
-            dW += self.backwardPass() #save all weight gradients
+            #backprops and adds to weights
+            dW += self.backwardPass()
         #avg all gradients of the minibatch
         dW = dW / numData
         #update weights
         self.w1.updateGrad(self.stepSize, dW)
 
 
+    #defines the accuracy of the network on the test data (10k images/labels)
+    #returns the accuracy as a decimal value (1.0 is 100%)
     def accuracy(self, testImages, testLabels):
-        accuracy = 0.0 #accuracy starts at 0, adds 1 for each correctly identified image, then takes a %
-        for i in range(len(testImages)): #checks accuracy for all test data
+        #accuracy starts at 0, adds 1 for each correctly identified image
+        accuracy = 0.0
+        #loops through all test data
+        for i in range(len(testImages)):
+            #forward props to get scores, stored as self.h1
             self.forwardPass(testImages[i], testLabels[i])
             largestValue = 0 #index of largest value
-            for j in range(1, len(self.h1)): #finds the index of the largest score
+             #finds the index of the largest score
+            for j in range(1, len(self.h1)):
+                #if there is a new larger value, updates index
                 if (self.h1[j] > self.h1[largestValue]):
                     largestValue = j
-            if (largestValue == testLabels[i]): #if the largest score is the correct class, add accuracy
+            #if the largest score is the correct class, add accuracy
+            if (largestValue == testLabels[i]):
                 accuracy += 1           
         #compute % accuracy
         return accuracy / len(testImages)
 
-        
+
+    #to be coded
     def outputData(self):
         #output loss
         #output accuracy
@@ -63,36 +88,62 @@ class Network():
         pass
 
 
+    #trains the network. Takes in train data and optional batch size.
+    #outputs data on the network each minibatch.
     def train(self, trainImages, trainLabels, batchSize = 25):
-        #trains data using minibatches
+        #Defines number of minibatches. If the minibatch isn't divisible by the
+        #data size, it will round down and not run on all the train data.
+        #Should be updated to be randomly ordered.
+        numMinibatches = int(len(trainImages)/batchSize)
+        #creates an index to use for slicing
+        dataIndex = 0
         for i in range(int(len(trainImages)/batchSize)):
-            miniBatchImages = trainImages[i:i+batchSize] #slices train data
-            miniBatchLabels = trainLabels[i:i+batchSize]
+            #slices train images and labels
+            miniBatchImages = trainImages[dataIndex:dataIndex+batchSize]
+            miniBatchLabels = trainLabels[dataIndex:dataIndex+batchSize]
+            #updates dataIndex
+            dataIndex += batchSize
+            #trains the minibatch
             self.trainBatch(miniBatchImages, miniBatchLabels)
-            self.outputData() #some output data
+            #outputs data for analysis
+            self.outputData()
 
 
 #uses the python-mnist module to import the data
-def importData(dir = './mnist'): #imports from a given directory, predefined to /mnist
+#imports from a given directory, predefined to /mnist
+def importData(dir = './mnist'):
+    #creates mndata object from mnist data
     mndata = MNIST(dir)
+    #loads train data
     trainImages, trainLabels = mndata.load_training()
+    #loads test data
     testImages, testLabels = mndata.load_testing()
-    return trainImages, trainLabels, testImages, testLabels #returns as 4 lists
+    #returns as 4 lists
+    return trainImages, trainLabels, testImages, testLabels
+
 
 
 def main():
-    numberNet = Network() #init network
-    trainImages, trainLabels, testImages, testLabels = importData() #imports data
-    initialAccuracy = numberNet.accuracy(testImages, testLabels) #test initial accuracy
+     #init network
+    numberNet = Network()
+    #import data
+    trainImages, trainLabels, testImages, testLabels = importData()
+    #test initial accuracy
+    initialAccuracy = numberNet.accuracy(testImages, testLabels)
+    #tracks time to train the data. Later to be added to outputData method instead
     startTime = time.perf_counter()
-    numberNet.train(trainImages, trainLabels) #train network
+    #trains the network
+    numberNet.train(trainImages, trainLabels)
+    #tracks end time for training
     endTime = time.perf_counter()
     #pickle weights for re-use.
-    finalAccuracy = numberNet.accuracy(testImages, testLabels) #test final accuracy
+    ##to be implemeneted
+    #tests final accuracy
+    finalAccuracy = numberNet.accuracy(testImages, testLabels)
     #display output data
-
     print('initAccuracy: ', initialAccuracy)
     print('finalAccuracy: ', finalAccuracy)
     print('Train time: ' , endTime - startTime)
+
 
 main()
