6d476d23c3be9b2fee2292f64e9ead02bce8d088
Jared Grambihler01 <jaredgrambihler@gmail.com>
2019-05-14 10:40:55 -0700

    Continued reorginization - not functional.

--- NumberNet/Layers/Layers.py
+++ NumberNet/Layers/Layers.py
@@ -3,47 +3,89 @@ import math
 import ActivationFunctions
 
 class Layer:
-    #Layer class to define input, weights, activation function
+    """
+    Class to be defined for each layer of the network.
+    """
+    #Layer format
+    #input - (maybe?) allocate to store forward pass input
+    #Weights
+    #Biases (can move to weights later to optimize)
+    #Activation Function (can be None)
 
-    def __init__(self, inputSize, outputSize, bias = False, activationFunction = ""):
-        #code from old create layer method
-        self.input = []
+    def __init__(self, inputSize, outputSize, bias = False, activationFunction = None):
+        #make inputs take parameters?
+        self.input = np.array(inputSize, outputSize)
         self.weigths = []
         self.bias = []
-        if(activationFunction != ""):
+        #Sets activation function
+        if(activationFunction):
             if activationFunction == 'ReLU':
                 self.activationFunction = ActivationFunctions.ReLU()
             elif activationFunction == 'Sigmoid':
                 self.activationFunction = ActivationFunctions.Sigmoid()
             elif activationFunction == 'LeakyReLU' or activationFunction == 'HipsterReLU':
                 self.activationFunction = ActivationFunctions.LeakyReLU()
+        else:
+            self.actiavtionFuntion = None
 
-    def forwardPass(self):
-        pass
+    #FORWARD AND BACKWARD PASS FROM MULTIPLICATION LAYER
+    def forwardPass(self, weights, inputVector):
+        """
+        stores weights and input for the backward pass.
+        Returns a dot product between them
+        """
+        self._weights = weights.weights
+        self.input = inputVector
+        return np.dot(self._weights, self.input)
 
     def backwardPass(self, priorGradient):
-        pass
+        """
+        Computes gradients of both the weights and the input
+        Returns weightGrad, inputGrad
+        """
+        #creates matrices of the proper size to hold the gradients
+        weightGrad = np.ones(np.shape(self._weights))
+        inputGrad = np.ones(np.shape(self.input))
+
+        #creates the weightGrad
+        for i in range(len(self.input)):
+            #mutiplies the input at i to the entire row
+            #of weights it ends up impacting
+            tempGrad = self.input[i]
+            weightGrad[: , i] = tempGrad
+        for i in range(len(priorGradient)):
+            #takes the priorGradient at i and multiplies
+            #it to the entire row that was a part of it's result
+            weightGrad[i] *= priorGradient[i]
+
+        #creates the inputGrad
+        for i in range(len(self.input)):
+            #takes the sum of the weightsRow that impacted the inputGrad's effect
+            weightRow = self._weights[:, i]
+            #multiplies together the row each priorGradient that it was related t0
+            tempGrad = np.multiply(weightRow, priorGradient)
+            #takes the sum of the 'i' row impact as the local gradient 
+            #(already multiplied to the prior gradient)
+            inputGrad[i] = np.sum(tempGrad)
+
+        return weightGrad, inputGrad
 
 
 class Weights:
-    """
-    Class to handle weights in the network
-    """
-
+    """Class for weights on each layer"""
     def __init__(self, inputSize, outputSize):
-        ##REPLACE X AND Y WITH INPUTSIZE AND OUTPUTSIZE
         """
         Creates weights based on the input size.
         x = number of outputs
         y = number of inputs
         """
         #guassian distribution w/ sd of sqrt(2/inputs)
-        self._weights = np.random.randn(x*y) * math.sqrt(2.0/y)
-        self._weights = np.reshape(self._weights, (x,y))
+        self._weights = np.random.randn(inputSize*outputSize) * math.sqrt(2.0/outputSize)
+        self._weights = np.reshape(self._weights, (inputSize,outputSize))
 
         #learning params
-        self.vdw = np.zeros((x,y))
-        self.sdw = np.zeros((x,y))
+        self.vdw = np.zeros((inputSize,outputSize))
+        self.sdw = np.zeros((inputSize,outputSize))
 
 
     def updateGrad(self, grad, parameters):
@@ -76,7 +118,7 @@ class Weights:
 
 
 
-class Bias(Layer):
+class Bias:
     """
     Layer for biases.
     Should be implemented after weights and input are multiplied together.
@@ -132,7 +174,7 @@ class Bias(Layer):
 
 
 
-class Multiplication(Layer):
+class Multiplication:
     """
     Multiplies together two matrices
     weights = weights matrix
