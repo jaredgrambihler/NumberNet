cb2fa4e3ac35f3ab8101c0e165a61521d49d6201
JaredGrambihlerHome <jaredgrambihler@gmail.com>
2020-05-20 12:11:26 -0700

    Added batch training test

--- Tests/testNetwork.py
+++ Tests/testNetwork.py
@@ -12,11 +12,11 @@ class NetworkTests(unittest.TestCase):
     def setUp(self):
         """
         WEIGHTS       BIAS  ACTIVATION    PARAMETERS
-        .22 .24 .44   .2    Softmax       stepSize: 1e-3
+        .22 .24 .44   .2    Softmax       stepSize: .1
         .33 .42 .68   .3                  regularization: 1e-5
                                           decay: .9
         """
-        params = Parameters(stepSize=1e-3, regularization=1e-5, decay=.9, RMSProp=False, momentum=False)
+        params = Parameters(stepSize=.1, regularization=1e-5, decay=0, RMSProp=False, momentum=False)
         layers = [Layer(3,2,True)]
         lossFunc = Softmax()
         self.network = Network(parameters=params, layers=layers, lossFunction=lossFunc)
@@ -78,15 +78,36 @@ class NetworkTests(unittest.TestCase):
             self.assertAlmostEqual(weightsGrad[i], x, 4, msg="Error in weigts gradient")
         #TODO - find a crafty way to check the output gradient
 
-    @unittest.skip("Unwritten Test")
     def testAccuracy(self):
-        pass
+        """
+        WEIGHTS       BIAS  ACTIVATION
+        .22 .24 .44   .2    Softmax      
+        .33 .42 .68   .3                  
+        INPUT           Output         
+        .1 .2 .3   -->  .44546  .55453                   
+        """
+        testImages = np.array([[.1,.2,.3], [.1,.2,.3], [.1,.2,.3]])
+        testLabels = [0,1,1] #correct label is 1
+        accuracy = self.network.accuracyTest(testImages, testLabels)
+        self.assertAlmostEqual(2/3, accuracy, msg="Inaccurate accuracy")
 
-    @unittest.skip("Unwritten Test")
     def testBatchTraining(self):
-        pass
+        """
+        WEIGHTS GRADIENT            STEP SIZE ADJUSTMENT
+         .04454  .08909  .13363     .004454  .008909  .013363
+        -.04454 -.08909 -.13363    -.004454 -.008909 -.013363
 
-    @unittest.skip("Unwritten Test")
-    def testTraining(self):
-        pass
-
+        WEIGHTS                     ADJUSTED WEIGHTS
+        .22 .24 .44                 .215546  .231091  .426637
+        .33 .42 .68                 .334454  .428909  .693363
+        """
+        #set normalized data to not do anything to image so math is the same
+        #as prior tests
+        self.network.setNormalizeData(np.array([[0,0,0]]))
+        #run back on input
+        self.network.trainBatch(np.array([self.input]), np.array([self.label]), 0)
+        #check to see if weights updated correctly
+        expected = np.array([.215546, .231091, .426637, .334454, .428909, .693363])
+        actual = self.network.layers[0].weights._weights.reshape(6)
+        for i, x in enumerate(expected):
+            self.assertAlmostEqual(x, actual[i], 4, msg="Improper weights update in train batch")
